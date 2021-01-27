3113c2d176735e30315cae4a2b32e7e55860a5f3
Serhii <73497221+Sersh4745@users.noreply.github.com>
2021-01-27 23:44:30 +0200

    Update task_1.py

--- hw.1/task_1.py
+++ hw.1/task_1.py
@@ -21,29 +21,13 @@ c2: 4
 -2 -2 3 Yes 0.82 -1.82
 '''
 
-import math
-
 print("Введите коэффициенты для уровнения ax^2 + bx + c = 0: ")
 
 a = float(input("a = "))
 b = float(input("b = "))
 c = float(input("c = "))
-
-# discr = b ** 2 - 4 * a * c
-# print("Дискриминант D = %.2f" % discr)
-
-# if discr > 0:
-#     x1 = (-b + math.sqrt(discr)) / (2 * a)
-#     x2 = (-b - math.sqrt(discr)) / (2 * a)
-#     print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
-# elif discr == 0:
-#     x = -b / (2 * a)
-#     print("x = %.2f" % x)
-# else:
-#     print("Корней нет") 
-
 discriminant = b**2 - 4*a*c
-print('Дискриминант = ' + discriminant)
+print('Дискриминант = ' + str(discriminant))
 if discriminant < 0:
     print('Корней нет')
 elif discriminant == 0:
@@ -53,4 +37,3 @@ else:
     x1 = (-b + discriminant ** 0.5) / (2 * a)
     x2 = (-b - discriminant ** 0.5) / (2 * a)
     print('x1 = ' + str(x1)+ '\nx2 = ' + str(x2))
-   
