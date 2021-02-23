a61e3ac777b2b48489c9df88e1f5e09ba1569150
Alexei Shiyan <shian@triolcorp.com>
2021-02-24 01:35:53 +0200

    Внес правки по hw.2. Посмотри!

--- hw.2/book.py
+++ hw.2/book.py
@@ -1,12 +1,12 @@
 class Book:
-    def __init__(self, book_id, book_name, book_author, book_year, id_reader=None):
+    def __init__(self, book_id: int, book_name: str,
+                 book_author: str, book_year: int):
+
         self.id = book_id
         self.title = book_name
         self.author = book_author
         self.year = book_year
 
-        # сюда будем писать id юзера,
-        # который взял книгу
         self.id_reader = None
 
     def __repr__(self):
--- hw.2/library.py
+++ hw.2/library.py
@@ -5,9 +5,10 @@ import os.path
 
 
 class Library:
-    def __init__(self, book_list = None, reader_list = None):
-        self.books = book_list
-        self.readers = reader_list
+    def __init__(self, book_list=None, reader_list=None):
+        # тернарный оператор =)
+        self.books = book_list if book_list is not None else []
+        self.readers = reader_list if reader_list is not None else []
 
     def init_from_files(self, books_file: str = None, readers_file: str = None) -> None:
         """
@@ -17,9 +18,6 @@ class Library:
         :param readers_file: файл со списком читателей
         :return: None
         """
-        if self.books is None: self.books = []
-        if self.readers is None: self.readers = []
-
         if books_file is not None and os.path.exists(books_file):
             with open(books_file, 'r', encoding='utf-8') as f:
                 for line in f:
@@ -30,7 +28,7 @@ class Library:
                             int(data_list[0]),  # id книги
                             data_list[1],       # название книги
                             data_list[2],       # автор книги
-                            data_list[3],       # год издания
+                            int(data_list[3]),  # год издания
                         ))
 
         if readers_file is not None and os.path.exists(readers_file):
@@ -38,33 +36,32 @@ class Library:
                 for line in file:
                     name_list = line.strip().split('$!$')
 
-                    if len(name_list) == 4 and name_list[0].isnumeric():
+                    # name_list[0] -> int, т.к. id
+                    # name_list[3] -> int, т.к. age
+                    if len(name_list) == 4 \
+                            and name_list[0].isnumeric() \
+                            and name_list[3].isnumeric():
                         self.readers.append(Reader(
                             int(name_list[0]),  # id 
-                            name_list[1],       # имя
-                            name_list[2],       # фамилия
-                            name_list[3],       # возраст
+                            name_list[1],  # имя
+                            name_list[2],  # фамилия
+                            int(name_list[3]),  # возраст
                         ))
 
     def add_book(self, obj_book: Book) -> None:
         """
-        # @todo напиши коммент
+        Добавление книги в Библиотеку
 
-        :param obj_book:
-        :return:
+        :param obj_book: объект Книга
+        :return: None
         """
         all_books_id = [book.id for book in self.books]
-        if obj_book[0] in all_books_id:
-            print(f'Ошибка , книга с таким ID "{obj_book[0]}" уже есть в библиотеке! ')
+        if obj_book.id in all_books_id:
+            print(f'Ошибка , книга с таким ID "{obj_book.id}" уже есть в библиотеке! ')
             return
 
-        self.books.append(Book(
-                            int(obj_book[0]),  # id книги
-                            obj_book[1],       # название книги
-                            obj_book[2],       # автор книги
-                            obj_book[3],       # год издания
-                        ))
-        print(f'Поздравляем, книга "{obj_book[0]}" добавлена в библиотеку')
+        self.books.append(obj_book)
+        print(f'Поздравляем, книга "{obj_book.id}" добавлена в библиотеку')
 
     def remove_book(self, id_book: int) -> None:
         """
@@ -73,71 +70,66 @@ class Library:
         :param id_book: id книги, которую нужно удалить
         :return: None
         """
-        if id_book.isnumeric():
-            all_books_id = [book.id for book in self.books]
-            if int(id_book) not in all_books_id:
-                print(f'Ошибка, книга с таким ID  "{id_book}" не найдена')
-            for book in self.books:
-                if int(book.id) == int(id_book):
-                    self.books.remove(book)
-                    print(f'Книга  "{book.title}" была удалена')
-        else:
-            print('Введите числом')
-                
+        all_books_id = [book.id for book in self.books]
+        if id_book not in all_books_id:
+            print(f'Ошибка, книга с ID "{id_book}" не найдена')
+            return
+
+        for book in self.books:
+            if book.id == id_book:
+                self.books.remove(book)
+                print(f'Книга "{book.title}" была удалена')
+
+                # ок, а если эта книга у кого-то из читателей???
+                # хотя ты и не добавлял книги читателям
+                for reader in self.readers:
+                    if id_book in reader.books:
+                        reader.books.remove(book)
+                        break
+
+                break
+
     def add_reader(self, obj_reader: Reader) -> None:
         """
-        # @todo Коммент!
+        Добавление читателя в библиотеку
 
-        :param obj_reader:
-        :return:
+        :param obj_reader: Объект Читатель
+        :return: None
         """
         all_readers_id = [reader.id for reader in self.readers]
-        if obj_reader[0] in all_readers_id:
-            print(f'Ошибка, читатель с таким ID "{obj_reader[0]}" уже существует!')
+        if obj_reader.id in all_readers_id:
+            print(f'Ошибка, читатель с ID "{obj_reader.id}" уже существует!')
             return
-        #self.readers.append(obj_reader)
-        self.readers.append(Reader(
-                            int(obj_reader[0]),  # id 
-                            obj_reader[1],       # имя
-                            obj_reader[2],       # фамилия
-                            obj_reader[3],       # возраст
-                        ))
-        print(f'Поздравляем, читатель с ID {obj_reader[0]} зарегестрирован.')
+
+        self.readers.append(obj_reader)
+        print(f'Поздравляем, читатель с ID {obj_reader.id} зарегистрирован.')
 
     def print_all_books(self) -> None:
         """
         Выводит список всех книг, которые зарегистрированы в библиотеке
-
-        :return: None
         """
-        print('Список книг :')
+        print('Список книг:')
         for book in self.books:
             print(book)
 
     def print_list_books_available(self) -> None:
         """
         Выводит список книг, которые находятся в библиотеке
-
-        :return: None
         """
         print('Спсиок книг в наличии:')
 
         for book in self.books:
-            if book.id_reader == None:
+            if book.id_reader is None:
                 print(book)
-
 
     def print_list_books_not_available(self) -> None:
         """
         Выводит список книг, которые находятся на руках у читателей
-
-        :return: None
         """
         print('Спсиок книг не в наличии:')
         for book in self.books:
-            if book.id_reader != None:
+            if book.id_reader is not None:
                 print(book)
-
 
     def give_book(self, id_book: int, id_reader: int) -> None:
         """
@@ -145,26 +137,33 @@ class Library:
 
         :param id_book: id книги
         :param id_reader: id читателя
-        :return:
         """
-        if id_book.isnumeric() and id_reader.isnumeric():
-            all_books_id = [book.id for book in self.books]
-            if int(id_book) not in all_books_id:
-                print(f'Ощибка, книги с таким ID {id_book} нет!')
+        all_books_id = [book.id for book in self.books]
+        if id_book not in all_books_id:
+            print(f'Ошибка, книги с ID {id_book} нет!')
+            return
 
-            all_readers_id = [reader.id for reader in self.readers]
-            if int(id_reader) not in all_readers_id:
-                print(f'Ошибка, пользователя с таким ID {id_reader} нет!')
+        all_readers_id = [reader.id for reader in self.readers]
+        if id_reader not in all_readers_id:
+            print(f'Ошибка, пользователя с ID {id_reader} нет!')
+            return
 
-            for book in self.books:
-                for reader in self.readers:
-                    if book.id == int(id_book):
-                        if reader.id == int(id_reader):
-                            book.id_reader = int(id_reader)
-                            print('Поздравляем книга выдана')
-        else:
-            print('Введите числом')
-       
+        # твои циклы очень долгие, лучше отдельно!
+        obj_book = [book for book in self.books if id_book == book.id][0]
+
+        # и третья проверка =)
+        if obj_book.id_reader is not None:
+            print(f'Ошибка, книги с ID {id_reader} нет в наличии!')
+            return
+
+        # получаем юзера
+        obj_reader = [reader for reader in self.readers if id_reader == reader.id][0]
+
+        # выдаем книгу
+        obj_book.id_reader = obj_reader.id
+        obj_reader.books.append(obj_book.id)
+        print('Поздравляем, книга выдана!')
+
     def return_book(self, id_book: int, id_reader: int) -> None:
         """
         Функция возврата книги в библиотеку
@@ -173,13 +172,27 @@ class Library:
         :param id_reader: id читателя
         :return:
         """
-        if id_book.isnumeric() and id_reader.isnumeric():
-            for book in self.books:
-                if int(book.id) == int(id_book):
-                    if int(book.id_reader) == int(id_reader):
-                        book.id_reader = None    
-                        print('Поздравляем Вы вернули книгу')
-                    else:
-                        print(f'Ощибка, книги с таким ID {id_book} нет!')
-        else:
-            print('Введите числом')
+        all_books_id = [book.id for book in self.books]
+        if id_book not in all_books_id:
+            print(f'Ошибка, книги с ID {id_book} нет!')
+            return
+
+        all_readers_id = [reader.id for reader in self.readers]
+        if id_reader not in all_readers_id:
+            print(f'Ошибка, пользователя с ID {id_reader} нет!')
+            return
+
+        obj_book = [book for book in self.books if id_book == book.id][0]
+        if obj_book.id_reader is None:
+            print(f'Ошибка, книга с ID {id_reader} и так в библиотеке!')
+            return
+
+        obj_reader = [reader for reader in self.readers if id_reader == reader.id][0]
+        if obj_book.id_reader != obj_reader.id or obj_book.id not in obj_reader.books:
+            print(f'Ошибка, книга с ID {id_reader} находится не у {obj_reader.name}!')
+            return
+
+        # возвращаем книгу
+        obj_book.id_reader = None
+        obj_reader.books.remove(obj_book.id)
+        print('Поздравляем Вы вернули книгу')
