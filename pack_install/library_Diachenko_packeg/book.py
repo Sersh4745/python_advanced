2b9013cd58d43f06d6a416e27ad9be2a13b568cb
Serhii <73497221+Sersh4745@users.noreply.github.com>
2021-02-27 19:34:57 +0200

    v1

---
+++ pack_install/library_Diachenko_packeg/book.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self, book_id: int, book_name: str,
+                 book_author: str, book_year: int):
+
+        self.id = book_id
+        self.title = book_name
+        self.author = book_author
+        self.year = book_year
+
+        self.id_reader = None
+
+    def __repr__(self):
+        return f'{self.id}) {self.title}, {self.year}'
